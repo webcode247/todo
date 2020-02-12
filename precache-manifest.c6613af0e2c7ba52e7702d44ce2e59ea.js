006027702d97b24585c1edae3572d3ccfc79b159
webcode <webcde@gmail.com>
2020-02-12 01:59:45 +0100

    Updates

---
+++ precache-manifest.c6613af0e2c7ba52e7702d44ce2e59ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44985fc401131e30328318a10b689317",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "cf65d74f4388bd7f0ee9",
+    "url": "/todo-app/static/css/main.9e904beb.chunk.css"
+  },
+  {
+    "revision": "750cccaf4b8debba1266",
+    "url": "/todo-app/static/js/2.484eb360.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-app/static/js/2.484eb360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf65d74f4388bd7f0ee9",
+    "url": "/todo-app/static/js/main.1d769cab.chunk.js"
+  },
+  {
+    "revision": "029f20ec000abbf3b1a5",
+    "url": "/todo-app/static/js/runtime-main.475c18f9.js"
+  }
+]);
